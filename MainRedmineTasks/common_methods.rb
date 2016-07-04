90332852ae9b0c94161c547837962077b1c0ca44
Oksana Yemchenko <oye@247e.com>
2016-07-05 00:01:04 +0300

    Fixed: one assert per test, include module, no excessive logouts, no complex locators

--- MainRedmineTasks/common_methods.rb
+++ MainRedmineTasks/common_methods.rb
@@ -1,142 +1,145 @@
 require 'selenium-webdriver'
 
-def register_new_user (driver, login, password)
-  wait = Selenium::WebDriver::Wait.new(:timeout=>10)
-  home_page_url = 'http://demo.redmine.org/'
+module CommonMethods
+  def register_new_user (login, password)
+    wait = Selenium::WebDriver::Wait.new(:timeout=>10)
+    home_page_url = 'http://demo.redmine.org/'
 
-  driver.navigate.to (home_page_url)
-  wait.until{driver.find_element(:class, 'register').displayed?}
-  driver.find_element(:class, 'register').click
-  wait.until{driver.find_element(:id, 'user_login').displayed?}
+    @driver.navigate.to (home_page_url)
+    wait.until{@driver.find_element(:class, 'register').displayed?}
+    @driver.find_element(:class, 'register').click
+    wait.until{@driver.find_element(:id, 'user_login').displayed?}
 
-  driver.find_element(:id, 'user_login').send_keys(login)
-  driver.find_element(:id, 'user_password').send_keys(password)
-  driver.find_element(:id, 'user_password_confirmation').send_keys(password)
-  driver.find_element(:id, 'user_firstname').send_keys(login+'FirstName')
-  driver.find_element(:id, 'user_lastname').send_keys(login+'LastName')
-  driver.find_element(:id, 'user_mail').send_keys(login+'@mail.mail')
-  driver.find_element(:css, '#user_language option[value="uk"]').click
+    @driver.find_element(:id, 'user_login').send_keys(login)
+    @driver.find_element(:id, 'user_password').send_keys(password)
+    @driver.find_element(:id, 'user_password_confirmation').send_keys(password)
+    @driver.find_element(:id, 'user_firstname').send_keys(login+'FirstName')
+    @driver.find_element(:id, 'user_lastname').send_keys(login+'LastName')
+    @driver.find_element(:id, 'user_mail').send_keys(login+'@mail.mail')
+    @driver.find_element(:css, '#user_language option[value="uk"]').click
 
-  driver.find_element(:name, 'commit').click
-  wait.until{driver.find_element(:id, 'flash_notice').displayed?}
-end
-
-def log_in (driver, login, pass)
-  wait = Selenium::WebDriver::Wait.new(:timeout=>10)
-
-  driver.find_element(:class, 'login').click
-  wait.until{driver.find_element(:id, 'username').displayed?}
-  driver.find_element(:id, 'username').send_keys(login)
-  driver.find_element(:id, 'password').send_keys(pass)
-  driver.find_element(:name, 'login').click
-end
-
-def log_out (driver)
-  wait = Selenium::WebDriver::Wait.new(:timeout=>10)
-
-  driver.find_element(:class, 'logout').click
-  wait.until{driver.find_element(:class, 'login').displayed?}
-end
-
-def change_password (driver, pass1, pass2)
-  wait = Selenium::WebDriver::Wait.new(:timeout=>10)
-
-  driver.find_element(:class, 'my-account').click
-  driver.find_element(:css, '.icon.icon-passwd').click
-  wait.until{driver.find_element(:id, 'password').displayed?}
-
-  driver.find_element(:id, 'password').send_keys(pass1)
-  driver.find_element(:id, 'new_password').send_keys(pass2)
-  driver.find_element(:id, 'new_password_confirmation').send_keys(pass2)
-  driver.find_element(:name, 'commit').click
-  wait.until{driver.find_element(:id, 'flash_notice').displayed?}
-end
-
-def create_new_project (driver)
-  wait = Selenium::WebDriver::Wait.new(:timeout=>10)
-
-  driver.find_element(:css, 'a.projects').click
-  wait.until{driver.find_element(:css, 'a.icon.icon-add').displayed?}
-  driver.find_element(:css, 'a.icon.icon-add').click
-  wait.until{driver.find_element(:css, 'input[type="submit"][name="commit"]').displayed?}
-
-  driver.find_element(:css, '#project_name').send_keys(project_name = get_random_project_name)
-  driver.find_element(:css, '#project_description').send_keys('Some description.')
-  driver.find_element(:css, '#project_identifier').send_keys(get_random_project_identifier)
-  driver.find_element(:css, 'input[type="submit"][name="commit"]').click
-  wait.until{@driver.find_element(:id, 'flash_notice').displayed?}
-
-  project_name
-end
-
-def navigate_to_project (driver, project_name)
-  wait = Selenium::WebDriver::Wait.new(:timeout=>10)
-
-  driver.find_element(:css, '.user.active').click
-  wait.until{driver.find_element(:css, 'a[href*="/projects/"]').displayed?}
-
-  # have to add foreach for case with list of projects OR probably doesn't work at all
-  project_link = driver.find_element(:css, 'a[href*="/projects/"]')
-  project_link_name = driver.find_element(:css, 'a[href*="/projects/"]').text
-  if project_link_name == project_name
-    project_link.click
+    @driver.find_element(:name, 'commit').click
+    wait.until{@driver.find_element(:id, 'flash_notice').displayed?}
   end
 
-  wait.until{driver.find_element(:css, 'a.overview').displayed?}
-end
+  def log_in (login, pass)
+    wait = Selenium::WebDriver::Wait.new(:timeout=>10)
 
-def create_project_version (driver)
-  wait = Selenium::WebDriver::Wait.new(:timeout=>10)
+    @driver.find_element(:class, 'login').click
+    wait.until{@driver.find_element(:id, 'username').displayed?}
+    @driver.find_element(:id, 'username').send_keys(login)
+    @driver.find_element(:id, 'password').send_keys(pass)
+    @driver.find_element(:name, 'login').click
+  end
 
-  driver.find_element(:css, '.settings.selected').click
-  wait.until{driver.find_element(:css, '#tab-info.selected').displayed?}
-  driver.find_element(:css, '#tab-versions').click
-  driver.find_element(:css, 'a.icon.icon-add[href*="versions"]').click
+  def log_out
+    wait = Selenium::WebDriver::Wait.new(:timeout=>10)
 
-  driver.find_element(:css, '#version_name').send_keys(get_random_project_version)
-  driver.find_element(:css, 'input[type="submit"][name="commit"]').click
-  wait.until{@driver.find_element(:id, 'flash_notice').displayed?}
-end
+    @driver.find_element(:class, 'logout').click
+    wait.until{@driver.find_element(:class, 'login').displayed?}
+  end
 
-def create_new_issue (driver, issue_type)
-  wait = Selenium::WebDriver::Wait.new(:timeout=>10)
-  issue_topic = 'Some ' + issue_type + ' topic.'
+  def change_password (pass1, pass2)
+    wait = Selenium::WebDriver::Wait.new(:timeout=>10)
 
-  driver.find_element(:css, 'a.new-issue').click
-  wait.until{driver.find_element(:css, 'a.new-issue.selected').displayed?}
+    @driver.find_element(:class, 'my-account').click
+    wait.until{@driver.find_element(:css, '.contextual').displayed?}
+    @driver.find_element(:css, '.icon.icon-passwd').click
+    wait.until{@driver.find_element(:id, 'password').displayed?}
 
-  issue_types_dropdown_menu = driver.find_element(:css, 'select#issue_tracker_id')
-  option = Selenium::WebDriver::Support::Select.new(issue_types_dropdown_menu)
-  option.select_by(:text, issue_type)
+    @driver.find_element(:id, 'password').send_keys(pass1)
+    @driver.find_element(:id, 'new_password').send_keys(pass2)
+    @driver.find_element(:id, 'new_password_confirmation').send_keys(pass2)
+    @driver.find_element(:name, 'commit').click
+    wait.until{@driver.find_element(:id, 'flash_notice').displayed?}
+  end
 
-  sleep(1)
+  def create_new_project
+    wait = Selenium::WebDriver::Wait.new(:timeout=>10)
 
-  driver.find_element(:css, 'input#issue_subject').send_keys(issue_topic)
+    @driver.find_element(:css, 'a.projects').click
+    wait.until{@driver.find_element(:css, 'a.icon.icon-add').displayed?}
+    @driver.find_element(:css, 'a.icon.icon-add').click
+    wait.until{@driver.find_element(:css, 'input[type="submit"][name="commit"]').displayed?}
 
-  sleep(1)
+    @driver.find_element(:css, '#project_name').send_keys(project_name = get_random_project_name)
+    @driver.find_element(:css, '#project_description').send_keys('Some description.')
+    @driver.find_element(:css, '#project_identifier').send_keys(get_random_project_identifier)
+    @driver.find_element(:css, 'input[type="submit"][name="commit"]').click
+    wait.until{@driver.find_element(:id, 'flash_notice').displayed?}
 
-  driver.find_element(:css, 'input[type="submit"][name="commit"]').click
-  wait.until{driver.find_element(:id, 'flash_notice').displayed?}
+    project_name
+  end
 
-  issue_id = driver.find_element(:css, 'div#flash_notice a').text.to_s.slice(1..-1)
-end
+  def navigate_to_project (project_name)
+    wait = Selenium::WebDriver::Wait.new(:timeout=>10)
 
-def get_random_login
-  random = Random.new
-  'OksanaTestLogin'+random.rand(99999).to_s
-end
+    @driver.find_element(:css, '.user.active').click
+    wait.until{@driver.find_element(:css, 'a[href*="/projects/"]').displayed?}
 
-def get_random_project_name
-  random = Random.new
-  'OksanaTestProject'+random.rand(999).to_s
-end
+    # ToDo: add foreach for case with list of projects
+    project_link = @driver.find_element(:css, 'a[href*="/projects/"]')
+    project_link_name = @driver.find_element(:css, 'a[href*="/projects/"]').text
+    if project_link_name == project_name
+      project_link.click
+    end
 
-def get_random_project_identifier
-  random = Random.new
-  'oksanatestprojectidentifier'+random.rand(99999).to_s
-end
+    wait.until{@driver.find_element(:css, 'a.overview').displayed?}
+  end
 
-def get_random_project_version
-  random = Random.new
-  'OksanaTestProjectVersion'+random.rand(999).to_s
+  def create_project_version
+    wait = Selenium::WebDriver::Wait.new(:timeout=>10)
+
+    @driver.find_element(:css, '.settings.selected').click
+    wait.until{@driver.find_element(:css, '#tab-info.selected').displayed?}
+    @driver.find_element(:css, '#tab-versions').click
+    @driver.find_element(:css, 'a.icon.icon-add[href*="versions"]').click
+
+    @driver.find_element(:css, '#version_name').send_keys(get_random_project_version)
+    @driver.find_element(:css, 'input[type="submit"][name="commit"]').click
+    wait.until{@driver.find_element(:id, 'flash_notice').displayed?}
+  end
+
+  def create_new_issue (issue_type)
+    wait = Selenium::WebDriver::Wait.new(:timeout=>10)
+    issue_topic = 'Some ' + issue_type + ' topic.'
+
+    @driver.find_element(:css, 'a.new-issue').click
+    wait.until{@driver.find_element(:css, 'a.new-issue.selected').displayed?}
+
+    issue_types_dropdown_menu = @driver.find_element(:css, 'select#issue_tracker_id')
+    option = Selenium::WebDriver::Support::Select.new(issue_types_dropdown_menu)
+    option.select_by(:text, issue_type)
+
+    sleep(1)
+
+    @driver.find_element(:css, 'input#issue_subject').send_keys(issue_topic)
+
+    sleep(1)
+
+    @driver.find_element(:css, 'input[type="submit"][name="commit"]').click
+    wait.until{@driver.find_element(:id, 'flash_notice').displayed?}
+
+    @driver.find_element(:css, 'div#flash_notice a').text.to_s.slice(1..-1)
+  end
+
+  def get_random_login
+    random = Random.new
+    'OksanaTestLogin'+random.rand(99999).to_s
+  end
+
+  def get_random_project_name
+    random = Random.new
+    'OksanaTestProject'+random.rand(999).to_s
+  end
+
+  def get_random_project_identifier
+    random = Random.new
+    'oksanatestprojectidentifier'+random.rand(99999).to_s
+  end
+
+  def get_random_project_version
+    random = Random.new
+    'OksanaTestProjectVersion'+random.rand(999).to_s
+  end
 end
--- MainRedmineTasks/test_redmine.rb
+++ MainRedmineTasks/test_redmine.rb
@@ -4,157 +4,137 @@ require_relative 'common_methods'
 
 class TestRedmine < Test::Unit::TestCase
 
+  include CommonMethods
+
   def setup
     @driver = Selenium::WebDriver.for :firefox
     @wait = Selenium::WebDriver::Wait.new(:timeout=>10)
+
+    @login = get_random_login
     @password1 = 'Qwerty123!'
     @password2 = 'Qwerty123?'
-    @login = get_random_login
-    @home_page_url = 'http://demo.redmine.org/'
   end
 
   def test_registration_successful
-    register_new_user(@driver, @login, @password1)
+    register_new_user(@login, @password1)
 
     success_text = 'Your account has been activated. You can now log in.'
     actual_text = @driver.find_element(:id, 'flash_notice').text
+
     assert_equal(success_text, actual_text)
   end
 
   def test_can_logout
-    register_new_user(@driver, @login, @password1)
-    log_out(@driver)
+    register_new_user(@login, @password1)
+    log_out
 
-    assert_equal(@home_page_url, @driver.current_url, 'Logout did not redirect to home page')
-    assert (@driver.find_element(:class, 'login').displayed?)
+    assert(@driver.find_element(:class, 'login').displayed?, 'Login button is not displayed after logout.')
   end
 
   def test_can_login
-    register_new_user(@driver, @login, @password1)
-    log_out(@driver)
-    log_in(@driver, @login, @password1)
+    register_new_user(@login, @password1)
+    log_out
+    log_in(@login, @password1)
 
     logged_in_user = @driver.find_element(:css, '#loggedas .user.active').text
-    assert_equal(@login, logged_in_user)
-    assert (@driver.find_element(:class, 'my-account').displayed?)
+
+    assert_equal(@login, logged_in_user, 'Logged in user is not displayed as active.')
   end
 
   def test_password_change
-    register_new_user(@driver, @login, @password1)
-    log_out(@driver)
-    log_in(@driver, @login, @password1)
-
-    @driver.find_element(:class, 'my-account').click
-    @driver.find_element(:css, '.icon.icon-passwd').click
-
-    @driver.find_element(:id, 'password').send_keys(@password1)
-    @driver.find_element(:id, 'new_password').send_keys(@password2)
-    @driver.find_element(:id, 'new_password_confirmation').send_keys(@password2)
-
-    @driver.find_element(:name, 'commit').click
-    @wait.until{@driver.find_element(:id, 'flash_notice').displayed?}
+    register_new_user(@login, @password1)
+    change_password(@password1, @password2)
 
     success_text = 'Пароль успішно оновлений.'
     actual_text = @driver.find_element(:id, 'flash_notice').text
+
     assert_equal(success_text, actual_text)
   end
 
   def test_can_login_new_password
-    register_new_user(@driver, @login, @password1)
-    log_out(@driver)
-    log_in(@driver, @login, @password1)
-    change_password(@driver, @password1, @password2)
-    log_out(@driver)
-    log_in(@driver, @login, @password2)
+    register_new_user(@login, @password1)
+    change_password(@password1, @password2)
+    log_out
+    log_in(@login, @password2)
 
     logged_in_user = @driver.find_element(:css, '#loggedas .user.active').text
-    assert_equal(@login, logged_in_user)
-    assert (@driver.find_element(:class, 'my-account').displayed?)
+
+    assert_equal(@login, logged_in_user, 'Logged in user is not displayed as active after password change.')
   end
 
   def test_can_create_new_project
-    register_new_user(@driver, @login, @password1)
-    create_new_project(@driver)
+    register_new_user(@login, @password1)
+    create_new_project
 
     success_text='Створення успішно завершене.'
     actual_text = @driver.find_element(:id, 'flash_notice').text
+
     assert_equal(success_text, actual_text)
   end
 
   def test_create_project_version
-    register_new_user(@driver, @login, @password1)
-    create_new_project(@driver)
-    create_project_version(@driver)
+    register_new_user(@login, @password1)
+    create_new_project
+    create_project_version
 
     success_text='Створення успішно завершене.'
     actual_text = @driver.find_element(:id, 'flash_notice').text
+
     assert_equal(success_text, actual_text)
   end
 
   def test_can_create_new_bug
     issue_type = 'Bug'
 
-    register_new_user(@driver, @login, @password1)
-    project_name = create_new_project(@driver)
-    navigate_to_project(@driver, project_name)
-    issue_id = create_new_issue(@driver, issue_type)
-
-    actual_text = @driver.find_element(:id, 'flash_notice').text
-    assert_true(actual_text.to_s.include?('Issue'))
-    assert_true(actual_text.to_s.include?('created.'))
-    assert_true(@driver.find_element(:css, 'div#content h2').text.to_s.include?(issue_type))
-    assert_true(@driver.find_element(:css, 'div.subject h3').text.to_s.include?(issue_type))
+    register_new_user(@login, @password1)
+    project_name = create_new_project
+    navigate_to_project(project_name)
+    issue_id = create_new_issue(issue_type)
 
     @driver.find_element(:css, 'a.issues').click
-    created_issue_id = @driver.find_element(:css, 'table[class*="list issues"] tbody > *:first-child td.id').text
-    created_issue_type = @driver.find_element(:css, 'table[class*="list issues"] tbody > *:first-child td.tracker').text
+    @wait.until{@driver.find_element(:css, '.list.issues').displayed?}
 
-    assert_equal(issue_id, created_issue_id)
-    assert_equal(issue_type, created_issue_type)
+    created_issue = @driver.find_element(:id, 'issue-'+issue_id.to_s)
+    created_issue_type = created_issue.find_element(:class, 'tracker').text
+
+    assert_equal(issue_type, created_issue_type,
+                 'Type of created issue does not match the type selected during creation')
     end
 
   def test_can_create_new_feature
     issue_type = 'Feature'
 
-    register_new_user(@driver, @login, @password1)
-    project_name = create_new_project(@driver)
-    navigate_to_project(@driver, project_name)
-    issue_id = create_new_issue(@driver, issue_type)
-
-    actual_text = @driver.find_element(:id, 'flash_notice').text
-    assert_true(actual_text.to_s.include?('Issue'))
-    assert_true(actual_text.to_s.include?('created.'))
-    assert_true(@driver.find_element(:css, 'div#content h2').text.to_s.include?(issue_type))
-    assert_true(@driver.find_element(:css, 'div.subject h3').text.to_s.include?(issue_type))
+    register_new_user(@login, @password1)
+    project_name = create_new_project
+    navigate_to_project(project_name)
+    issue_id = create_new_issue(issue_type)
 
     @driver.find_element(:css, 'a.issues').click
-    created_issue_id = @driver.find_element(:css, 'table[class*="list issues"] tbody > *:first-child td.id').text
-    created_issue_type = @driver.find_element(:css, 'table[class*="list issues"] tbody > *:first-child td.tracker').text
+    @wait.until{@driver.find_element(:css, '.list.issues').displayed?}
 
-    assert_equal(issue_id, created_issue_id)
-    assert_equal(issue_type, created_issue_type)
+    created_issue = @driver.find_element(:id, 'issue-'+issue_id.to_s)
+    created_issue_type = created_issue.find_element(:class, 'tracker').text
+
+    assert_equal(issue_type, created_issue_type,
+                 'Type of created issue does not match the type selected during creation')
   end
 
   def test_can_create_new_support
     issue_type = 'Support'
 
-    register_new_user(@driver, @login, @password1)
-    project_name = create_new_project(@driver)
-    navigate_to_project(@driver, project_name)
-    issue_id = create_new_issue(@driver, issue_type)
-
-    actual_text = @driver.find_element(:id, 'flash_notice').text
-    assert_true(actual_text.to_s.include?('Issue'))
-    assert_true(actual_text.to_s.include?('created.'))
-    assert_true(@driver.find_element(:css, 'div#content h2').text.to_s.include?(issue_type))
-    assert_true(@driver.find_element(:css, 'div.subject h3').text.to_s.include?(issue_type))
+    register_new_user(@login, @password1)
+    project_name = create_new_project
+    navigate_to_project(project_name)
+    issue_id = create_new_issue(issue_type)
 
     @driver.find_element(:css, 'a.issues').click
-    created_issue_id = @driver.find_element(:css, 'table[class*="list issues"] tbody > *:first-child td.id').text
-    created_issue_type = @driver.find_element(:css, 'table[class*="list issues"] tbody > *:first-child td.tracker').text
+    @wait.until{@driver.find_element(:css, '.list.issues').displayed?}
 
-    assert_equal(issue_id, created_issue_id)
-    assert_equal(issue_type, created_issue_type)
+    created_issue = @driver.find_element(:id, 'issue-'+issue_id.to_s)
+    created_issue_type = created_issue.find_element(:class, 'tracker').text
+
+    assert_equal(issue_type, created_issue_type,
+                 'Type of created issue does not match the type selected during creation')
   end
 
   def teardown
